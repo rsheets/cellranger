07e7d0cb106f7d021bcef5e2851ab0db024140a4
jennybc <jenny@stat.ubc.ca>
2016-04-15 15:44:12 -0700

    re-document column letter <--> number

--- R/letter-to-from-num.R
+++ R/letter-to-from-num.R
@@ -1,14 +1,26 @@
-#' Convert column IDs from letter representation to integer
+#' Convert between letter and integer representations of column IDs
 #'
-#' @param x character vector of letter-style column IDs (case insensitive)
+#' Convert "A1"-style column IDs from a letter representation to an integer,
+#' e.g. column A becomes 1, column D becomes 4, etc. Or go the other way around.
 #'
-#' @return vector of positive integers, giving associated column numbers
+#' \itemize{
+#' \item Google Sheets have up to 300 columns (column KN).
+#' \item Excel 2010 spreadsheets have up to 16,384 columns (column XFD).
+#' \item ZZ is column 702.
+#' \item ZZZ is column 18,278 (no known spreadsheet actually goes that high).
+#' }
+#' @name letter-num-conversion
 #'
+#' @param x a character vector of "A1" style column IDs (case insensitive)
+#' @param y a vector of integer column IDs
+#' @return a vector of column IDs, either character or integer
+NULL
+
+#' @rdname letter-num-conversion
 #' @examples
 #' letter_to_num('Z')
 #' letter_to_num(c('AA', 'ZZ', 'ABD', 'ZZZ'))
 #' letter_to_num(c(NA, ''))
-#'
 #' @export
 letter_to_num <- function(x) {
 
@@ -22,28 +34,17 @@ letter_to_num <- function(x) {
 
 }
 
-#' Convert column numbers to letter representation
-#'
-#' @param y vector of column numbers
-#'
-#' @return character vector of letter-style column IDs
-#'
+#' @rdname letter-num-conversion
 #' @examples
 #' num_to_letter(28)
 #' num_to_letter(900)
 #' num_to_letter(18278)
 #' num_to_letter(c(25, 52, 900, 18278))
 #' num_to_letter(c(NA, 0, 4.8, -4))
-#'
 #' @export
 num_to_letter <- function(y) {
 
   stopifnot(is.numeric(y))
-
-  # FYI Google spreadsheets have 300 columns max
-  # Excel 2010 spreadsheets have up to 16,384 columns
-  #  ZZ <-->    702
-  # ZZZ <--> 18,278
 
   # fcn to express column number in this weird form of base 26
   jfun <- function(div) {
@@ -52,7 +53,7 @@ num_to_letter <- function(y) {
     }
 
     ret <- integer()
-    while(div > 0) {
+    while (div > 0) {
       remainder <- ((div - 1) %% 26) + 1
       ret <- c(remainder, ret)
       div <- (div - remainder) %/% 26
